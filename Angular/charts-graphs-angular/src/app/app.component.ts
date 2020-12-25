83236677d18eb77051f2886de043b495f091ebe2
bouguern mohamed <bouguern.mohamed.10@gmail.com>
2020-12-25 12:37:49 +0100

    Performance Chart

--- Angular/charts-graphs-angular/src/app/app.component.ts
+++ Angular/charts-graphs-angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import * as CanvasJS from '../assets/canvasjs.min';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ export class AppComponent implements OnInit{
 
   ngOnInit() {
     this.columnChart();
+    this.performanceChart();
   }
 
   columnChart() {
@@ -40,6 +42,35 @@ export class AppComponent implements OnInit{
     chart.render();
   }
 
+  performanceChart() {
+    let dataPoints = [];
+	  let y = 0;		
+	  for ( var i = 0; i < 10000; i++ ) {		  
+		  y += Math.round(5 + Math.random() * (-5 - 5));	
+		  dataPoints.push({ y: y});
+    }
+
+	  let chart = new CanvasJS.Chart("chartContainer2", {
+		  zoomEnabled: true,
+		  animationEnabled: true,
+		  exportEnabled: true,
+		  title: {
+			  text: "Performance Demo - 10000 DataPoints"
+		  },
+		  subtitles: [{
+			  text: "Try Zooming and Panning"
+		  }],
+		  data: [
+		    {
+			    type: "line",                
+			    dataPoints: dataPoints
+		    }]
+	  });
+		
+	  chart.render();
+  }
+
   
+
 
 }
